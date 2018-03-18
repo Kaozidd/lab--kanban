8d991b0a67e1aa54109c9e08a094426d47a620b7
kaozidd <arthur_hemingway@outlook.com>
2018-03-18 05:08:32 -0600

    adding data

---
+++ lab--kanban/src/data/datasource.js
@@ -0,0 +1,91 @@
+export default [{
+  key: 1,
+  title: 'To Do',
+  tasks: [{
+    key: 1,
+    title: 'New mobile application for iOS and Android. Versions for phones and tablets.',
+    tagged: '.client'
+  }, {
+    key: 2,
+    title: 'Prepare quote for website development for Netflix Company.',
+    tagged: '.client'
+  }, {
+    key: 3,
+    title: 'Prepare assets and source files for developers with new ToDo web application',
+    tagged: '.internal'
+  }, {
+    key: 4,
+    title: 'Create design of case studies for clients.',
+    tagged: '.internal'
+  }]
+}, {
+  key: 2,
+  title: 'In Progress',
+  tasks: [{
+    key: 1,
+    title: 'Prepare copy for case study ASDF',
+    tagged: '.internal'
+  }, {
+    key: 2,
+    title: 'Configure new Magento store and implement payments.',
+    tagged: '.client'
+  }, {
+    key: 3,
+    title: 'Prepare quote for website development for client AwesomeCompany and some longer title for task.',
+    tagged: '.internal'
+  }]
+}, {
+  key: 3,
+  title: 'Done',
+  tasks: [{
+    key: 1,
+    title: 'Create design of case studies for clients.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 2,
+    title: 'Push mobile application to App Store and Google Play.',
+    tagged: '.client',
+    done: true
+  }, {
+    key: 3,
+    title: 'Final tests of ToDo web application.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 4,
+    title: 'Prepare invoices for clients.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 5,
+    title: 'Create design of case studies for clients.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 6,
+    title: 'Create design of case studies for clients.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 7,
+    title: 'Push mobile application to App Store and Google Play.',
+    tagged: '.client',
+    done: true
+  }, {
+    key: 8,
+    title: 'Final tests of ToDo web application.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 9,
+    title: 'Prepare invoices for clients.',
+    tagged: '.internal',
+    done: true
+  }, {
+    key: 10,
+    title: 'Create design of case studies for clients.',
+    tagged: '.internal',
+    done: true
+  }]
+}];
