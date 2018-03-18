9995e5b5a2701be1d6f0ed792519d8bc39234487
kaozidd <arthur_hemingway@outlook.com>
2018-03-18 06:17:44 -0600

    adding submit functionality

--- lab--kanban/src/App.js
+++ lab--kanban/src/App.js
@@ -4,6 +4,19 @@ import Header from './components/Header'
 import Board from './components/Board';
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+  addCard = (e) => {
+    e.preventDefault();
+    console.log(this.state.value)
+  }
   render() {
     return (
       <div>
@@ -11,9 +24,14 @@ class App extends Component {
         <section>
           <div className='board'>
             <div className='board__form container'>
-              <form>
+              <form onSubmit={this.addCard} name='form'>
                 <div className='form'>
-                  <input ref='newItem' className='form__text' type='text' placeholder='Add new item...' />
+                  <input ref='newItem' 
+                    className='form__text' 
+                    type='text' 
+                    placeholder='Add new item...'
+                    value={this.state.value}
+                    onChange={this.handleChange} />
                   <button className='form__submit' type='submit'>Add new</button>
                 </div>
               </form>
